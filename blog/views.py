fe4d6fb51abaf50a3534a7538e8801966d5b4286
shiree0121 <shirewhite@hotmail.com>
2026-01-12 11:19:35 +0000

    updated blog templates and added blog urls and views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(status=1).order_by('-created_on')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, status=1)
+    return render(request, 'blog/post_detail.html', {'post': post})
