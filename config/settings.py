9f7d23d6452fba0c8d89368a60fe2f5e228673cf
shiree0121 <shirewhite@hotmail.com>
2026-01-14 17:50:25 +0000

    Use PostgreSQL database via DATABASE_URL

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 import os
+import dj_database_url
+
 
 if os.path.isfile('env.py'):
     import env
@@ -86,13 +88,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
