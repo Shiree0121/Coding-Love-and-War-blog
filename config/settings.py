11f62a2752d8f60cc1ff94719af8ff8ce4b4aaf0
shiree0121 <shirewhite@hotmail.com>
2026-01-23 15:21:32 +0000

    Fix LOGIN_URL to use custom user_login view instead of default accounts/login

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Login URL configuration
+LOGIN_URL = 'user_login'
